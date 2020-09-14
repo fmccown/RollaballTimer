a77dbdc9fd10c94085561b62223408327b17173e
Frank McCown <fmccown@harding.edu>
2020-09-14 14:24:57 -0500

    Add project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public int totalPickups = 8;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+    private bool gameOver;
+    private Timer timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+
+        //winTextObject.SetActive(false);
+        winTextObject.SetText("");
+
+        gameOver = false;
+
+        // Get reference to timer
+        timer = GameObject.FindObjectOfType<Timer>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count;
+
+        if (count == totalPickups)
+        {
+            winTextObject.SetText("You win!!");
+            gameOver = true;
+
+            // Stop the timer
+            timer.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.tag);
+        if (gameOver)
+            return;
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NoMoreTime()
+    {
+        winTextObject.SetText("You Lose!");
+        gameOver = true;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    //public GameObject player;
+    public TextMeshProUGUI timerText;
+    public int totalSeconds = 12;
+
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = totalSeconds - Time.time - startTime;
+        string seconds = (t % 60).ToString("f1");
+
+        if (t <= 0)
+        {
+            //GameObject.Find("Player").SendMessage("NoMoreTime");
+            //player.SendMessage("NoMoreTime");
+            GameObject.FindObjectOfType<PlayerController>().NoMoreTime();
+        }
+        else
+        {
+            timerText.SetText(seconds);
+        }        
+    }
+
+}
